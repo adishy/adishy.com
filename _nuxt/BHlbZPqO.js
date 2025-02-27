e8c785bea5bc48632f1bf975b6221eb7b7302b1c
adishy <adishy@users.noreply.github.com>
2025-02-27 13:04:20 +0000

    deploy: 3f6bcf7389dd9fc00780a77b4b29cca5cfb1ddaf

---
+++ _nuxt/BHlbZPqO.js
@@ -0,0 +1,1 @@
+import{_ as s,c as o,o as r,y as n}from"./jtXH2Xw6.js";const t={};function c(e,a){return r(),o("ul",null,[n(e.$slots,"default")])}const _=Object.assign(s(t,[["render",c]]),{__name:"ProseUl"});export{_ as default};
