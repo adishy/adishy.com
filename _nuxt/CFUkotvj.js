efeeab8b51482768cd6ba1fed82a9c8b9267cc95
adishy <adishy@users.noreply.github.com>
2025-03-02 04:19:43 +0000

    deploy: 922c1feae3b92237f56626c1a49f0f5da9d60489

---
+++ _nuxt/CFUkotvj.js
@@ -0,0 +1,1 @@
+import{_ as o,c as t,o as a,y as n,a as r}from"./WeSS1-Dp.js";const c={},d={class:"px-4 py-10 m-auto sm:px-8 sm:rounded-lg max-w-4xl sm:shadow"};function l(e,s){return a(),t("div",d,[n(e.$slots,"default",{},()=>[s[0]||(s[0]=r("h1",{class:"text-2xl"},"Page not found",-1))])])}const f=o(c,[["render",l]]);export{f as default};
